348072936de935c7ce10fb0ef0d584f748d31350
Abdul Mohamed <aboodmm@gmail.com>
2018-09-08 11:31:02 -0700

    fix fatal bug. install tslint

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,9 +8,9 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   constructor(public navCtrl: NavController) {}
 
-const counter = 1;
+counter = 1;
 
-const inputs = [
+inputs = [
   { 
     label: 'Assignment 1',
     value: '',
